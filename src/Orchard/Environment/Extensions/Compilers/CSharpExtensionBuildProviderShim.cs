dd2cc03bf1484ebc06753e910dde49ffee60a5ef
kay.one <kay.one@gmail.com>
2012-04-14 09:42:01 -0700

    a1f00fd194b8

---
+++ src/Orchard/Environment/Extensions/Compilers/CSharpExtensionBuildProviderShim.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Compilation;
+
+namespace Orchard.Environment.Extensions.Compilers {
+    public class CSharpExtensionBuildProviderShim : BuildProvider, IShim {
+        private readonly CompilerType _codeCompilerType;
+
+        public CSharpExtensionBuildProviderShim() {
+            OrchardHostContainerRegistry.RegisterShim(this);
+
+            _codeCompilerType = GetDefaultCompilerTypeForLanguage("C#");
+
+            // define a precompilation flag so that module can adapt for older version
+            var orchardVersion = typeof (IDependency).Assembly.GetName().Version;
+            _codeCompilerType.CompilerParameters.CompilerOptions += string.Format("/define:ORCHARD_{0}_{1}", orchardVersion.Major, orchardVersion.Minor) ;
+        }
+
+        public IOrchardHostContainer HostContainer { get; set; }
+
+        public override CompilerType CodeCompilerType {
+            get {
+                return _codeCompilerType;
+            }
+        }
+
+        public override void GenerateCode(AssemblyBuilder assemblyBuilder) {
+            var context = new CompileExtensionContext {
+                VirtualPath = this.VirtualPath,
+                AssemblyBuilder =  new AspNetAssemblyBuilder(assemblyBuilder, this)
+            };
+            HostContainer.Resolve<IExtensionCompiler>().Compile(context);
+        }
+    }
+}
---
+++ src/Orchard/Mvc/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using Orchard.Utility.Extensions;
+
+namespace Orchard.Mvc.Extensions {
+    public static class UrlHelperExtensions {
+        public static string AbsoluteAction(this UrlHelper urlHelper, Func<string> urlAction) {
+            return urlHelper.MakeAbsolute(urlAction());
+        }
+
+        public static string AbsoluteAction(this UrlHelper urlHelper, string actionName) {
+            return urlHelper.MakeAbsolute(urlHelper.Action(actionName));
+        }
+
+        public static string AbsoluteAction(this UrlHelper urlHelper, string actionName, object routeValues) {
+            return urlHelper.MakeAbsolute(urlHelper.Action(actionName, routeValues));
+        }
+
+        public static string AbsoluteAction(this UrlHelper urlHelper, string actionName, string controller) {
+            return urlHelper.MakeAbsolute(urlHelper.Action(actionName, controller));
+        }
+
+        public static string AbsoluteAction(this UrlHelper urlHelper, string actionName, string controller, object routeValues) {
+            return urlHelper.MakeAbsolute(urlHelper.Action(actionName, controller, routeValues));
+        }
+
+        public static string MakeAbsolute(this UrlHelper urlHelper, string url, string baseUrl = null) {
+            if(String.IsNullOrEmpty(baseUrl)) {
+                baseUrl = urlHelper.RequestContext.HttpContext.Request.ToRootUrlString();
+            }
+
+            if(String.IsNullOrEmpty(url)) {
+                return baseUrl;
+            }
+
+            // remove any application path from the base url
+            var applicationPath = urlHelper.RequestContext.HttpContext.Request.ApplicationPath;
+            
+            // orchardlocal/foo/bar => /orchardlocal/foo/bar
+            if(!url.StartsWith("/")) {
+                url = "/" + url;
+            }
+            // /orchardlocal/foo/bar => foo/bar
+            if (url.StartsWith(applicationPath)) {
+                url = url.Substring(applicationPath.Length);
+            }
+
+            baseUrl = baseUrl.TrimEnd('/');
+            url = url.TrimStart('/');
+            
+            return baseUrl + "/" + url;
+        }
+    }
+}
